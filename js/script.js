76686729552c43d4ebf413009cc60bc0c9c940c9
Lisandro Guerra <lisandro.digital@gmail.com>
2023-02-13 20:24:12 +0000

    data input validation

--- js/script.js
+++ js/script.js
@@ -7,12 +7,15 @@ const weightInput = document.querySelector("#weight")
 const heightInput = document.querySelector("#height")
 
 
-// Events
 form.onsubmit = event => {
     event.preventDefault()
 
     const weight = weightInput.value
     const height = heightInput.value
+
+    const showAlertBox = notNumber(weight) || notNumber(height)
+
+    if (showAlertBox) return
 
     const result = bmi(weight, height)
     const msg = `Your BMI is ${result}`
@@ -22,7 +25,11 @@ form.onsubmit = event => {
 }
 
 
-// Functions
+function notNumber(value) {
+    return (isNaN(value) || value == "")
+}
+
+
 function bmi(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(2)
 }
